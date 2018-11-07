f9de755fe5b4d4adbd5c23e3a89b28bc5154b6cc
Ryan Daley <rdaley321@yahoo.com>
2018-11-07 10:42:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     print "#{season.to_s.capitalize}:"
     holiday.each do |key, supplies|
-      print " #{key.to_s.split("_")}: #{supplies.join(", ")}"
+      print " #{key.to_s.split("_").each{|word| word.capitalize}}: #{supplies.join(", ")}"
     end
   end
 end
